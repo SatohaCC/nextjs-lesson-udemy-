3b037fb0f851cafe81b8e940a246c0ee0666993e
SatohaCC <42375182+SatohaCC@users.noreply.github.com>
2022-06-20 20:11:49 +0900

    push test

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Layout from "../components/Layout";
 export default function Home() {
 	return (
 		<Layout title="Home">
-			<p className="text-4xl">Welcome to Nextjs</p>
+			<p className="text-4xl">Welcome to Nextjs ver2</p>
 		</Layout>
 	);
 }
